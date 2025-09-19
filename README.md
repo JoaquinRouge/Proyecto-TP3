3b3ece3af4d3d7bd93f460b11cfb58cbe2dbfb68
JoaquinRouge <Joarouge@gmail.com>
2025-09-19 12:31:43 -0300

    empty project

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyecto_tp3
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/proyecto_tp3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyecto_tp3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
